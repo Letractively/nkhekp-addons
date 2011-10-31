f63189863c02539b66e75cab18d16f3658478458
alexandre.lerat@gmail.com <alexandre.lerat@gmail.com@aa5d1e75-6ac9-1aee-0c60-81a42e840081>
2011-10-31 02:43:57 +0000

    Mise en place de l'administration

---
+++ trunk/Mod_AlertPost/modules/AlertePost/lang/french.lang.php
@@ -0,0 +1,66 @@
+<?php
+if (!defined("INDEX_CHECK"))
+{
+	exit('You can\'t run this file alone.');
+}
+define("_ADMINALERTEPOST", "Administration Alerte Post");
+define("_GLOBALPREF","Préférences Globales");
+define("_NEWPOSTPREF","Préf. Nouveau post");
+define("_REPLYPOSTPREF","Préf. Réponse post");
+define("_EDITPOSTPREF","Préf. &Eacute;dition post");
+define("_ACTIVATEALERT","Activer l'envoi de mail pour tous les types d'opérations");
+define("_ACTIVATEALERTPOST","Activer l'envoi de mail lors d'un nouveau post");
+define("_ACTIVATEALERTREPLY","Activer l'envoi de mail lors d'une réponse à un post");
+define("_ACTIVATEALERTEDIT","Activer l'envoi de mail lors de l'édition d'un post");
+define("_ALERTEPOSTGEN","Préférences générales");
+define("_ALERTEPOST","Préférences pour l'envoi de mail lors d'un nouveau post");
+define("_ALERTEPOSTREPLY","Préférences pour l'envoi de mail lors d'une réponse à un post");
+define("_ALERTEPOSTEDIT","Préférences pour l'envoi de mail lors de l'édition d'un post");
+define("_USERFOR","Pour");
+define("_PREFSEND","Préférences sauvegardées.");
+define("_SUBJECT","Sujet");
+define("_USERMESS","Message");
+define("_SEND","Envoyer");
+define("_BACK","Retour");
+define("_EMPTYFIELD","Assurez vous que tous les champs soient remplis");
+define("_SENDUSER","Inclure le pseudo du poster dans le message");
+define("_SENDTITRE","Inclure le titre du nouveau post dans le message");
+define("_SENDURL", "Inclure l'url du post dans le message");
+define("_MESSSEND","Message envoyé avec succès.");
+define("_PRIVATEMESS","Messages Privés");
+define("_OF","De");
+define("_THE","le");
+define("_WROTE","a ecrit");
+define("_REPLY","Répondre");
+define("_DEL","Supprimer");
+define("_NOSELECTMESS","Aucun message sélectionné");
+define("_MESSDEL","Message supprimé avec succès.");
+define("_DELETEMESS","Vous êtes sur le point de supprimer le message de");
+define("_DELETEMESSAGES","Vous êtes sur le point de supprimer les messages");
+define("_MESSAGESDEL","Messages supprimés avec succès.");
+define("_CONFIRM","Continuer ?");
+define("_BY","Par");
+define("_CLEARSUCCES","effacé avec succès.");
+define("_DELCONFIRM","Confirmer");
+define("_CANCEL","Annuler");
+define("_DELBOX","Sup");
+define("_FROM","Provenant de");
+define("_DATE","Date");
+define("_SEEDETAILUSER","Voir les détails de l'auteur");
+define("_READMESS","Lire message");
+define("_STATUS","Status");
+define("_READ","Lu");
+define("_NOTREAD","Non lu");
+define("_CHECKALL","Tout Cocher");
+define("_UNCHECKALL","Tout Décocher");
+define("_NOMESSPV","Vous n'avez pas de message...");
+define("_SENDNEWMESS","Nouveau");
+define("_MASSMP","Message Privé Massif");
+define("_ALLMEMBERS","Tous les membres");
+define("_SITEMEMBERS","Membres du site (Niveau 1)");
+define("_TEAMMEMBERS","Membres Team (Qui font partie d'une team)");
+define("_NIVOMEMBERS","Membres (Niveau ");
+define("_ADMINMEMBERS","Administrateurs (Niveau 9)");
+define("_PERSONALISE","Personnalisé");
+define("_AIDE","Maintener la touche ''CTRL'' sur votre clavier et cliquer sur les pseudo à qui vous vouler écrire.");
+?>
