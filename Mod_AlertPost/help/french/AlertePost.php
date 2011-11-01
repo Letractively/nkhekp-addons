31f7490451104d2d286f81dff1bd6719d52bea1d
alexandre.lerat <alexandre.lerat@gmail.com>
2011-11-01 02:24:58 +0000

    Mise en place de l'aide en Français

---
+++ Mod_AlertPost/help/french/AlertePost.php
@@ -0,0 +1,14 @@
+<?php header('Content-type: text/html; charset=iso-8859-1'); ?><html>
+<head>
+<title>Aides Administration</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body style="font-family: Verdana; font-size: 10px" link="#000000" vlink="#000000" alink="#000000" bgcolor="#FFFFFF">
+<center><big><b>Administration Alerte Post</b></big></center><br>
+<li><b>Pr&eacute;f&eacute;rences Globales</b> : Affiche les pr&eacute;f&eacute;rences de notification des mails en fonction du type de post dans le forum.<br>Vous pouvez activer 3 types de notification mail :<br><br>- Notification lors d'un nouveau post<br>- Notification lors d'une r&eacute;ponse &agrave; un post<br>- Notification lors d'une &eacute;dition de post<br><br>
+<li><b>Pr&eacute;f. Nouveau post</b> : Ce formulaire vous permet d'&eacute;diter les diff&eacute;rentes informations qui composeront le mail lors de la cr&eacute;ation d'un nouveau post.
+<br><br>
+<li><b>Pr&eacute;f. R&eacute;ponse post</b> : Ce formulaire vous permet d'&eacute;diter les diff&eacute;rentes informations qui composeront le mail lors de la r&eacute;ponse &agrave; un post.
+<br><br>
+<li><b>Pr&eacute;f. &Eacute;dition post</b> : Ce formulaire vous permet d'&eacute;diter les diff&eacute;rentes informations qui composeront le mail lors de l'&eacute;dition d'un post.</body></html>
