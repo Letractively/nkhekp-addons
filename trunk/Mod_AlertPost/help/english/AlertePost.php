ef449bfc8a0b082cbe58f1e728149b97f16190c8
alexandre.lerat@gmail.com <alexandre.lerat@gmail.com@aa5d1e75-6ac9-1aee-0c60-81a42e840081>
2011-11-07 21:03:55 +0000

    Ajout de l'aide en anglais

---
+++ trunk/Mod_AlertPost/help/english/AlertePost.php
@@ -0,0 +1,14 @@
+<?php header('Content-type: text/html; charset=iso-8859-1'); ?><html>
+<head>
+<title>Administration Help</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body style="font-family: Verdana; font-size: 10px" link="#000000" vlink="#000000" alink="#000000" bgcolor="#FFFFFF">
+<center><big><b>Alert Post Administration</b></big></center><br>
+<li><b>Global Preferences</b> : Show the preferences of announcement of e-mails according to the type of post in the forum.<br>You can activate 3 types(chaps) of e-mail announcement:<br><br>- Announcement during a new post<br>- Announcement during an answer to a post<br>- Announcement during an edition of post<br><br>
+<li><b>Add post preferences</b> : This form allows you to edit the various information which will compose the e-mail during the creation of a new post.
+<br><br>
+<li><b>Reply post preferences</b> : This form allows you to edit the various information which will compose the e-mail during the answer to a post.
+<br><br>
+<li><b>Edit post preferences</b> : This form allows you to edit the various information which will compose the e-mail during the edition of a post.</body></html>
